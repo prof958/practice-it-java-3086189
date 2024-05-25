8f9e7bc9bee32ffc17abc8428ec5b28286769a8d
Alp Güler <32702565+prof958@users.noreply.github.com>
2024-05-25 20:35:18 +0000

    Java Practice Course init

---
+++ _02_02/Ticket.java
@@ -0,0 +1,11 @@
+package _02_02;
+
+public class Ticket {
+
+  public Ticket() {
+  }
+
+  private String destination = new String();
+  private double price;
+  private boolean isReturn;
+}
--- _02_04/Ticket.java
+++ _02_04/Ticket.java
@@ -10,6 +10,30 @@ public class Ticket {
   private double price;
   private boolean isReturn;
 
+  public void setDestination(String destination){
+    this.destination = destination;
+  }
+
+  public void setPrice (double price){
+    this.price = price;
+  }
+
+  public void setIsReturn(boolean isReturn){
+    this.isReturn = isReturn;
+  }
+
+  public String getDestination(){
+    return destination;
+  }
+  
+  public double getPrice() {
+    return price;
+  }
+  
+  public boolean getIsReturn() {
+    return isReturn;
+  }
+
   // Add three methods to set the value of each field, called setDestination,
   // setPrice and setIsReturn.
 
